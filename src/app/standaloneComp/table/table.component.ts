edcb33b8a10ac7fe4b558bce8e7c86957f5d2da1
AkromeDev <jcbackenddev@gmail.com>
2025-07-11 14:01:29 +0200

    Amazing, I am at Angular 20, that was pretty fast :)

--- src/app/standaloneComp/table/table.component.ts
+++ src/app/standaloneComp/table/table.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-table',
+    standalone: true,
     imports: [CommonModule],
     templateUrl: './table.component.html',
     styleUrls: ['./table.component.scss']
