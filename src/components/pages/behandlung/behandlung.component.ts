e64b5fbc91e8533f8cc56a06e2ce7a4371ea6dba
AkromeDev <jcbackenddev@gmail.com>
2025-07-14 23:11:28 +0200

    allllright, one more page completely done, tomorrow I will begin and finish the price pagem and begin the time calculator I guess, Then I am fucking done! let's goooooo

--- src/components/pages/behandlung/behandlung.component.ts
+++ src/components/pages/behandlung/behandlung.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ButtonItem } from 'src/models/ButtonItem';
 import { ImageHeroComponent } from "src/components/molecules/image-hero/image-hero.component";
 import { ImageTextBlockComponent } from "src/components/molecules/image-text-block/image-text-block.component";
-import { ButtonComponent } from "src/components/atoms/button/button.component";
 
 @Component({
   selector: 'app-behandlung',
   standalone: true,
-  imports: [ImageHeroComponent, ImageTextBlockComponent, ButtonComponent],
+  imports: [ImageHeroComponent, ImageTextBlockComponent],
   templateUrl: './behandlung.component.html',
   styleUrl: './behandlung.component.scss'
 })
@@ -22,14 +20,5 @@ export class BehandlungComponent implements OnInit {
     Sie ist für alle Hauttypen und Haarfarben geeignet und bietet eine sichere, effektive Lösung für unerwünschte Haare.
     
     Alle wichtigen Infos zur Elektrolyse findest du weiter unten.
-    Die Buttons führen dich direkt zu den häufig gestellten Fragen.
   `;
-
-
-  buttonList: ButtonItem[] = [
-    { label: 'Wie lang dauert es?', link: '#dauer', theme: 'dark' },
-    { label: 'Tut es Weh?', link: '#wehtun', theme: 'dark' },
-    { label: 'Ist es gesund?', link: '#gesund', theme: 'dark' }
-  ];
-
 }
