0e882584e3e80fead3194cf323a82899ca539acf
AkromeDev <jcbackenddev@gmail.com>
2025-07-18 17:26:49 +0200

    reversed to not lazy, it was not working on github pages

--- src/components/atoms/parallax/parallax.component.ts
+++ src/components/atoms/parallax/parallax.component.ts
@@ -1,50 +1,21 @@
-import {
-  Component,
-  Input,
-  ElementRef,
-  AfterViewInit,
-  OnDestroy,
-} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TitleComponent } from 'src/components/atoms/title/title.component';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
-  selector: 'app-parallax',
-  standalone: true,
-  imports: [CommonModule, TitleComponent],
-  templateUrl: './parallax.component.html',
-  styleUrls: ['./parallax.component.scss']
+    selector: 'app-parallax',
+    standalone: true,
+    imports: [CommonModule, TitleComponent],
+    templateUrl: './parallax.component.html',
+    styleUrls: ['./parallax.component.scss']
 })
-export class ParallaxComponent implements AfterViewInit, OnDestroy {
-  @Input() imageSrc: string = '';
-  @Input() title: string = '';
+export class ParallaxComponent implements OnInit {
+ @Input() imageSrc: string = "";
+ @Input() title: string = "";
 
-  isVisible: boolean = false;
-  private observer!: IntersectionObserver;
+  constructor() { }
 
-  constructor(private elRef: ElementRef) {}
-
-  ngAfterViewInit(): void {
-    this.observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          this.isVisible = true;
-          this.observer.disconnect();
-        }
-      },
-      {
-        rootMargin: '200px'
-      }
-    );
-
-    this.observer.observe(this.elRef.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.disconnect();
-    }
-  }
+  ngOnInit(): void { }
 
   getUrl() {
       return "url(" + this.imageSrc + ")";
